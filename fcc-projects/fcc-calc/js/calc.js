aac6d9f14dd645208d517230ddd58ec3c3feddd3
reaper47 <macpoule@gmail.com>
2016-05-29 20:33:31 -0400

    Worked on a calculator and weather app.

---
+++ fcc-projects/fcc-calc/js/calc.js
@@ -0,0 +1,136 @@
+ $(function() {
+	 let numSigns = 0;
+	 let numDots = 0;
+	 const numKeys = [40, 41, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+	 const signKeys = [95, 43, 42, 47, 37];
+	 let equalSignPressed = false;
+	 
+	 // Create the expression to evaluate from the calc's buttons.
+	 $('button').on('click', function() {
+		 const className = $(this).attr('class');
+		 const listText = $(this).text();
+		 
+		 if (equalSignPressed) {
+			 equalSignPressed = false;
+	     	 $('h4').text('');
+		 }
+		 
+		 if (className.match(/num/g)) { 
+		   $('h4').append(listText).css('font-size', '1.7rem');
+		   numSigns = 0;
+		 } else if (className.match(/sign/g) && numSigns === 0) {
+			 $('h3').append($('h4').text()).css('font-size', '1.3rem');
+			 $('h4').html('&nbsp;');
+			 checkSign(listText);
+			 numSigns = 1;
+			 numDots = 0;
+		 } else if (className.match(/dot/g) && numDots === 0) {
+			 $('h4').append(listText);
+			 numDots = 1;
+		 } else if (className.match(/clear/g)) {
+			 $('h3').html('&nbsp;');
+			 $('h4').html('&nbsp;');
+		 } else if (className.match(/clr-current/g)){
+			 $('h4').html('&nbsp;');
+		 } else if (className.match(/return/g)) {
+			  carriageReturn();
+		 }
+	 });
+	 
+	 // Evaluate the expression when the equal sign is pressed
+	 $('#equal-sign').on('click', function() {
+		 equalSignPressed = true;
+		 evaluateExpr();
+	 });
+	 
+	 // Enable keyboard expression manipulations
+	 $('html').on('keypress', function(e) {
+		 e.preventDefault();
+ 		 		 	
+		 if (equalSignPressed) {
+			 equalSignPressed = false;
+	     	 $('h4').text('');
+		 }
+		 
+		if (e.which === 61 && !$('h4').text().match(/= /g)) {
+		   	 equalSignPressed = true;			 
+			 $('h3').append($('h4').text());
+			 $('h4').html(' &nbsp;');
+			 evaluateExpr();
+		 } else if (e.which === 8) {
+			 carriageReturn();
+		 } else if (e.which === 96) {
+			  $('h4').html('&nbsp;');
+		 } else if (e.which === 0) {
+			  $('h3').html('&nbsp;');
+			  $('h4').html('&nbsp;');
+		} else if (e.which === 46) {
+			 $('h4').append();
+			 numDots = 1;
+	    } else {
+			 for (let key = 0; key < numKeys.length; key++) {
+		    	 if (e.which === numKeys[key]) {
+			    	 $('h3').append($('h4').text());
+		             $('h4').html(' &nbsp;');
+		   	    	 $('h4').text(numKeys[key]);
+					 break;
+		     	 }
+	    	 }
+			 
+			 for (key = 0; key < signKeys.length; key++) {
+				 if (e.which === signKeys[key]) {
+					 
+					 
+					 
+					 numSigns = 1;
+					 numDots = 0;
+					 break;
+				 }
+			 }			 
+		}
+	});
+	 
+	 function carriageReturn() {
+		const expression = $('h3').text();
+		$('h4').html('&nbsp;');
+		if (expression.length > 2) {
+		  $('h3').text(expression.substr(0, expression.length - 2));
+		}
+	 }
+	 
+	 function checkSign(type) {
+		 if (type === "x") {
+			 $('h3').append(' *');
+		 } else if (type === '÷') {
+			 $('h3').append(' /');
+		 } else {
+			 $('h3').append(' ' + type);
+		 }
+	 }
+	 
+	 function evaluateExpr() {
+		 // Evaluate the stuff inside the string 
+		 // expression and handle all kinds of errors
+		 try {  
+		   $('h3').append($('h4').text());
+		   $('h4').text('= ' + eval($('h3').text()));
+		 } catch(err) {  
+			 const errMsg = err.message;
+			 const expr = $('h3').text();
+	        			 
+			 if (errMsg === "identifier starts immediately after numeric literal" ||
+			      errMsg === "expected expression, got end of script" & expr.match(/\*|\+|\-|\//g) !== null ||
+				  errMsg === "expected expression, got '*'" ||
+				  errMsg === "unterminated regular expression literal") {
+			   $('h4').text("Error: Invalid sign position");
+			 } else if (errMsg === "missing ; before statement" && expr.match(/\)|\(/g) !== null || 
+							errMsg === "missing ) after argument list" && expr.match(/\)|\(/g) !== null  ||
+							errMsg === "expected expression, got ')'" && expr.match(/\)|\(/g) !== null || 
+							errMsg === "expected expression, got end of script" & expr.match(/\)|\(/g) !== null) {
+				 $('h4').text("Error: Brackets do not match.");
+			 } else if (errMsg.match(/is not a function/g)) {
+				 $('h4').text('Error: Missing multiplication sign');
+			 }
+		 }
+	 }
+ });
---
+++ fcc-projects/fcc-weather/js/weather.js
@@ -0,0 +1,37 @@
+/*
+User Story: I can see the weather in my current location.
+User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+User Story: I can push a button to toggle between Fahrenheit and Celsius.
+We recommend using the Open Weather API. This will require creating a free API key.
+*/
+
+const API_KEY = "cedf698587e8a1f379c0e0428718ffec";
+
+// Local city
+
+// 1. Detect longitude and latitude
+
+const lon;
+const lat;
+
+// 2. Get the html data
+
+// 3. Parse it using ajax and add it to the site
+
+// 4. Set up a background image according to the weather. Try using an image API
+
+// Any city or whatever
+ 
+// 0. Detect the type of search in order to know which weather fetch to use 
+ 
+// 1. Parse the html
+// ex: http://api.openweathermap.org/data/2.5/weather?q=London&mode=html&appid=cedf698587e8a1f379c0e0428718ffec
+
+
+// 2. Include an id to the temperature in order to toggle switch between C/Fahrenheit
+
+
+// 3. Add the html to the main page using ajax
+
+// 4. Replace the background image according to the weather. Try to use an image API
+
