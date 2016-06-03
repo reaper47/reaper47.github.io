8d2a6c0fbf0312e9df17b4a72bed0145cc748081
reaper47 <macpoule@gmail.com>
2016-06-03 11:17:27 -0400

    Fixed bug when calling the modal window.

--- js/content-panels/modal-init.js
+++ js/content-panels/modal-init.js
@@ -1,14 +1,15 @@
 (function() {
   if ($('#calc-info').length) {
-    createModal('#calc-info', 340, 300, false);
+    createModal('#calc-info', 340, 300, 'aside', false);
   } else if ($('#weather-info').length) {
-    createModal('#weather-info', 340, 300, true);
+    createModal('#weather-info', 340, 300, 'footer .right', true);
     
   }
   
-  function createModal(id, pxWidth, pxHeight, data) {
+  function createModal(id, pxWidth, pxHeight, target, data) {
     const $content = $(id).detach();
-    $('footer .right').on('click', function() {
+    
+    $(target).on('click', function() {
       modal.open({content: $content, width: pxWidth, height: pxHeight});
       if (data) {
         modal.addContent('#ul-info > ul > li', 'init');
