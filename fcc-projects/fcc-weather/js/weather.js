0bb65ccda32e55b05fd209984861c32de9a47831
reaper47 <macpoule@gmail.com>
2016-06-05 22:49:44 -0400

    Continued work on weather web app.

--- fcc-projects/fcc-weather/js/weather.js
+++ fcc-projects/fcc-weather/js/weather.js
@@ -1,8 +1,10 @@
 /*
-User Story: I can see the weather in my current location.
-User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
-User Story: I can push a button to toggle between Fahrenheit and Celsius.
-We recommend using the Open Weather API. This will require creating a free API key.
+TODO Implement weather image backgrounds
+TODO Implement search
+TODO Implement complete
+TODO Implement fail
+TODO Implement error
+TODO Catch all errors when fetching data
 */
 
 $(function() {
@@ -15,7 +17,7 @@ $(function() {
     .attr('datetime', today.year + '-' + today.month + '-' + today.day)
     .text(dob.toDateString());
     
-  // 2.1. Display current location data
+  // 2.1. Display data on the current location
   const elMap = document.getElementById('footer-area'),
         errMsg = 'Unable to fetch location.',
         errMsg2 = 'No data',
@@ -28,7 +30,7 @@ $(function() {
     elMap.textContent = errMsg;
   }
   
-  // 2.2. Display additional data in the modal window
+  // 2.2. Display additional data in the modal window and the footer
   let currentGeoData;
   $('footer .right').on('click', function() {
     const $modalList = $('#ul-info > ul > li');
@@ -65,158 +67,8 @@ $(function() {
     e.preventDefault();
 
   });
-
-  function displayData(data) {
-    const APIKEY = '892f2f3e695745ddcdca40b9c3e5561d',
-          LATITUDE = data['lat'],
-          LONGITUDE = data['long'],
-          UNITS = '?units=si';
-
-    $.ajax({
-      url: 'https://api.forecast.io/forecast/' + APIKEY + '/' + LATITUDE + ',' + LONGITUDE + UNITS,
-      type: 'GET',
-      data: {},
-      dataType: 'jsonp',
-      timeOut: 2000,
-      beforeSend: function(xhr) {
-        if (xhr.overrideMimeType) {
-          xhr.overrideMimeType("application/jsonp");
-        }
-        $('.loading').text('Loading...');
-      },
-      complete: function() {
-        //$('.loading').remove();
-      },
-      success: function(data) {
-        // Current forecast
-        $('#current-temp p:last-child')
-          .html(data.currently.summary + ', ' + '<span class="celsius">' + 
-                Math.round(data.currently.temperature) + 
-                tempUnitSign() + '</span>');
-        $('#current-weather-icon').addClass(data.currently.icon);
-        const $currentClass = $('#current-weather-icon').attr('class').split(' ');
-        skycons.set('current-weather-icon', $currentClass[$currentClass.length-1])
-        
-        // Today's forecast
-        $('#today-summary').text(data.daily.data[0].summary);
-        $('#today-wind p:nth-child(2)')
-          //.addClass('wind-kmph')
-          .html(windDirection(data.daily.data[0].windBearing) + 
-                '<span class="wind-kmph">' + 
-                Math.round(data.daily.data[0].windSpeed * 3.6) + 
-                unitSpeed() + '</span>');
-        $('#today-humidity p:nth-child(2)')
-          .text(data.daily.data[0].humidity * 100 + '%');
-        $('#today-sun p:first-child')
-          .text('Sunrise: ' + convertUnixTime(data.daily.data[0].sunriseTime), false);
-        $('#today-sun p:last-child')
-          .text('Sunset: ' + convertUnixTime(data.daily.data[0].sunsetTime), false);
-        $('#today-min-max p:first-child')
-          .addClass('celsius')
-          .html(Math.round(Math.round(data.daily.data[0].apparentTemperatureMin)) + tempUnitSign());
-        $('#today-min-max p:last-child')
-          .addClass('celsius')
-          .html(Math.round(Math.round(data.daily.data[0].apparentTemperatureMax)) + tempUnitSign());
-
-
-        // 2.3.1. Modify the class of the hourly forecast
-        $('.hourly-forecast > div > p.hourly-1').each(function(index, value) {
-          $(this).removeClass('hourly-1').addClass('hourly-' + (index + 1));
-        });
-
-        // 2.3.2. Modify the id of the hourly forecast
-        $('.hourly-forecast > div > canvas').each(function(index, value) {
-          $(this).attr('id', 'to-the-hour-icon' + (index+1));
-        });
-
-        // 2.3.3. Modify the id of the hourly precipiation forecast
-        $('.hourly-forecast > div > div > canvas').each(function(index, value) {
-          $(this).attr('id', 'hourly-icon' + (index+1));
-        });
-        
-        // Add data to the hourly forecast
-        for (let i = 1; i < 9; ++i) {
-         
-          // Add the hourly forecast time
-          const $unix_time = data.hourly.data[i].time;
-          const $time = convertUnixTime($unix_time, false);
-          $('.hourly-' + i).text($time);
-          
-          // Add the summary and the weather icon
-          $('#hour-' + i + ' > p:nth-child(2)').text(data.hourly.data[i].summary);
-          $('#hour-' + i + ' canvas').addClass(data.hourly.data[i].icon);
-          const $hourClasses = $('#to-the-hour-icon' + i).attr('class').split(' ');
-          skycons.set('to-the-hour-icon' + i, $hourClasses[$hourClasses.length-1]);
-          skycons.set('hourly-icon' + i, "rain");
-          
-          // Add the forecast temperature, precipitation and wind speed
-          $('#hour-' + i + ' > p:nth-child(4)')
-            .addClass('celsius')
-            .html(Math.round(data.hourly.data[i].apparentTemperature) + tempUnitSign());
-          $('#hour-' + i + ' > div:nth-child(5) canvas').addClass(data.hourly.data[i].precipType);
-          $('#hour-' + i + ' > div:nth-child(5) p')
-            .text(Math.round(data.hourly.data[i].precipProbability * 100) + '%');
-          try {
-            $('#hour-' + i + ' > p:nth-child(6)')
-              .html(windDirection(data.hourly.data[i].windBearing) + 
-                    '<span class="wind-kmph">' + 
-                    Math.round(data.hourly.data[i].windSpeed * 3.6) + 
-                    unitSpeed() + '</span>');
-          } catch(err) {
-            $('#hour-' + i + ' > p:nth-child(6)').text(errMsg2)
-          }
-        }
-        
-        // Modify the id of the daily weather icons
-        $('.future-forecast > div > div > canvas').each(function(index, value) {
-          $(this).attr('id', 'future-weather-icon' + (index+1));
-        });
-        
-        // 2.3.3. Modify the id of the daily precipitation forecast
-        $('.future-forecast > div > div > div > canvas').each(function(index, value) {
-          $(this).attr('id', 'daily-precip-icon' + (index+1));
-        });
-        
-        // Add data to the daily forecast
-        for (let i = 1; i < 5; ++i) {
-          // Add the day to the header
-          const $unix_time = data.daily.data[i].time;
-          const $date = convertUnixTime($unix_time, true);
-          $('#tomorrow-' + i + '> p' ).text($date.substr(0, 3) + ', ' + $date.substr(4, 6));
-          
-          // Add the icon and the summary data to the body
-          $('#tomorrow-' + i + '> div > canvas').addClass(data.daily.data[i].icon);
-          $('#tomorrow-' + i + '> div > p').text(data.daily.data[i].summary);
-          const $dailyClasses = $('#future-weather-icon' + i).attr('class').split(' ');
-          skycons.set('daily-precip-icon' + i, "rain");
-          skycons.set('future-weather-icon' + i, $dailyClasses[$dailyClasses.length-1]);
-          
-          // Add the temperature and chance-of-precipitation data to the footer
-          $('#tomorrow-' + i + ' div:last-child p.min')
-            .addClass('celsius')
-            .html(Math.round(data.daily.data[i].apparentTemperatureMin) + tempUnitSign());
-          $('#tomorrow-' + i + ' div:last-child p.max')
-            .addClass('celsius')
-            .html(Math.round(data.daily.data[i].apparentTemperatureMax) + tempUnitSign());
-          $('#tomorrow-' + i + ' div:last-child > div > img')
-            .addClass(data.daily.data[i].precipType);
-          $('#tomorrow-' + i + ' div:last-child > div > p')
-            .removeClass('celsius')
-            .text(Math.round(data.daily.data[i].precipProbability * 100) + '%');
-        }
-        
-        skycons.play();
-        console.log(data);
-      },
-      fail: function() {
-        $('body').html('<div id="fail-load">Failed</div>');
-      },
-      error: function(err) {
-        alert('shit');
-      }
-    });
-  }
   
+  // 4. Toggle between the imperial and metric system
   $('#units-form label').on('click', function() {
     const type = $(this).context.innerText;
     if (type === 'C') {
@@ -258,7 +110,147 @@ $(function() {
         .addClass('wind-mph');
     }
   });
-  
+
+  function displayData(data) {
+    const APIKEY = '892f2f3e695745ddcdca40b9c3e5561d',
+          LATITUDE = data['lat'],
+          LONGITUDE = data['long'],
+          UNITS = '?units=si';
+
+    $.ajax({
+      url: 'https://api.forecast.io/forecast/' + APIKEY + '/' + LATITUDE + ',' + LONGITUDE + UNITS,
+      type: 'GET',
+      data: {},
+      dataType: 'jsonp',
+      timeOut: 2000,
+      beforeSend: function(xhr) {
+        if (xhr.overrideMimeType) {
+          xhr.overrideMimeType("application/jsonp");
+        }
+        $('.loading').text('Loading...');
+      },
+      complete: function() {
+        // Implement stuff that will happen after the function is complete
+        // ..
+      },
+      success: function(data) {
+        // Current forecast
+        $('#current-temp p:last-child')
+          .html(data.currently.summary + ', ' + '<span class="celsius">' + 
+                Math.round(data.currently.temperature) + 
+                tempUnitSign() + '</span>');
+        $('#current-weather-icon').addClass(data.currently.icon);
+        const $currentClass = $('#current-weather-icon').attr('class').split(' ');
+        skycons.set('current-weather-icon', $currentClass[$currentClass.length-1])
+        
+        // Today's forecast
+        $('#today-summary').text(data.daily.data[0].summary);
+        $('#today-wind p:nth-child(2)')
+          //.addClass('wind-kmph')
+          .html(windDirection(data.daily.data[0].windBearing) + 
+                '<span class="wind-kmph">' + 
+                Math.round(data.daily.data[0].windSpeed * 3.6) + 
+                unitSpeed() + '</span>');
+        $('#today-humidity p:nth-child(2)')
+          .text(data.daily.data[0].humidity * 100 + '%');
+        $('#today-sun p:first-child')
+          .text('Sunrise: ' + convertUnixTime(data.daily.data[0].sunriseTime), false, false);
+        $('#today-sun p:last-child')
+          .text('Sunset: ' + convertUnixTime(data.daily.data[0].sunsetTime), false, false);
+        $('#today-min-max p:first-child')
+          .addClass('celsius')
+          .html(Math.round(Math.round(data.daily.data[0].apparentTemperatureMin)) + tempUnitSign());
+        $('#today-min-max p:last-child')
+          .addClass('celsius')
+          .html(Math.round(Math.round(data.daily.data[0].apparentTemperatureMax)) + tempUnitSign());
+
+
+        // 2.3.1. Modify the class of the hourly forecast
+        $('.hourly-forecast > div > p.hourly-1').each(function(index, value) {
+          $(this).removeClass('hourly-1').addClass('hourly-' + (index + 1));
+        });
+
+        // 2.3.2. Modify the id of the hourly forecast
+        $('.hourly-forecast > div > canvas').each(function(index, value) {
+          $(this).attr('id', 'to-the-hour-icon' + (index+1));
+        });
+        
+        // Add data to the hourly forecast
+        for (let i = 1; i < 9; ++i) {
+         
+          // Add the hourly forecast time
+          const $unix_time = data.hourly.data[i].time;
+          const $time = convertUnixTime($unix_time, false, false);
+          $('.hourly-' + i).text($time);
+          
+          // Add the summary and the weather icon
+          $('#hour-' + i + ' > p:nth-child(2)').text(data.hourly.data[i].summary);
+          $('#hour-' + i + ' canvas').addClass(data.hourly.data[i].icon);
+          const $hourClasses = $('#to-the-hour-icon' + i).attr('class').split(' ');
+          skycons.set('to-the-hour-icon' + i, $hourClasses[$hourClasses.length-1]);
+          
+          // Add the forecast temperature, precipitation and wind speed
+          $('#hour-' + i + ' > p:nth-child(4)')
+            .addClass('celsius')
+            .html(Math.round(data.hourly.data[i].apparentTemperature) + tempUnitSign());
+          $('#hour-' + i + ' > div:nth-child(5) canvas').addClass(data.hourly.data[i].precipType);
+          $('#hour-' + i + ' > div:nth-child(5) p')
+            .text(Math.round(data.hourly.data[i].precipProbability * 100) + '%');
+          try {
+            $('#hour-' + i + ' > p:nth-child(6)')
+              .html(windDirection(data.hourly.data[i].windBearing) + 
+                    '<span class="wind-kmph">' + 
+                    Math.round(data.hourly.data[i].windSpeed * 3.6) + 
+                    unitSpeed() + '</span>');
+          } catch(err) {
+            $('#hour-' + i + ' > p:nth-child(6)').text(errMsg2)
+          }
+        }
+        
+        // Modify the id of the daily weather icons
+        $('.future-forecast > div > div > canvas').each(function(index, value) {
+          $(this).attr('id', 'future-weather-icon' + (index+1));
+        });
+        
+        // Add data to the daily forecast
+        for (let i = 1; i < 5; ++i) {
+          // Add the day to the header
+          const $unix_time = data.daily.data[i].time;
+          const $date = convertUnixTime($unix_time, true, false);
+          $('#tomorrow-' + i + '> p' ).text($date.substr(0, 3) + ', ' + $date.substr(4, 6));
+          
+          // Add the icon and the summary data to the body
+          $('#tomorrow-' + i + '> div > canvas').addClass(data.daily.data[i].icon);
+          $('#tomorrow-' + i + '> div > p').text(data.daily.data[i].summary);
+          const $dailyClasses = $('#future-weather-icon' + i).attr('class').split(' ');
+          skycons.set('future-weather-icon' + i, $dailyClasses[$dailyClasses.length-1]);
+          
+          // Add the temperature and chance-of-precipitation data to the footer
+          $('#tomorrow-' + i + ' div:last-child p.min')
+            .addClass('celsius')
+            .html(Math.round(data.daily.data[i].apparentTemperatureMin) + tempUnitSign());
+          $('#tomorrow-' + i + ' div:last-child p.max')
+            .addClass('celsius')
+            .html(Math.round(data.daily.data[i].apparentTemperatureMax) + tempUnitSign());
+          $('#tomorrow-' + i + ' div:last-child > div > img')
+            .addClass(data.daily.data[i].precipType);
+          $('#tomorrow-' + i + ' div:last-child > div > p')
+            .removeClass('celsius')
+            .text(Math.round(data.daily.data[i].precipProbability * 100) + '%');
+        }
+        skycons.play();
+      },
+      fail: function() {
+        // Implement stuff that will happen if a fail occurs
+        // ..
+      },
+      error: function(err) {
+        // Implement stuff that will happen if an error occurs
+        // ..
+      }
+    });
+  }
+    
   function success(position) {
     const geoData = { 
             lat: position.coords.latitude, 
@@ -274,7 +266,7 @@ $(function() {
       if (xhr.status === 200) {
         responseObject = JSON.parse(xhr.responseText); 
 
-        if (responseObject.status !== "ZERO_RESULTS") {
+        if (responseObject.status === "OK") {
           const address = responseObject.results[0].address_components,
                 city = address[address.length - 3].long_name,
                 country = address[address.length - 2].long_name;
@@ -283,7 +275,7 @@ $(function() {
           displayData(geoData);
           currentGeoData = geoData;
         } else {
-          fail(errMsg);
+          fail(responseObject.status);
         }
       }
     }
@@ -293,19 +285,50 @@ $(function() {
 
   function fail(msg) { 
     elMap.textContent = errMsg;
+    $('#current-temp .loading').css('line-height', '2rem');
+    $('#current-temp .loading').css('margin-left', '-10rem');
+    $('.loading').text(errMsg2);
+    
+    if (msg === "ZERO_RESULTS") {
+      const noRes = "No results";
+      $('.loading').text(noRes);
+      $('#current-temp > p').append(noRes);
+      $('#current-temp .loading').css('margin-left', '-3rem');
+    } else if (msg === "OVER_QUERY_LIMIT") {
+      if (!Date.now) {
+        Date.now = function now() {
+          return new Date().getTime();
+        }
+      }
+      const hours = convertUnixTime(Date.now()/1000, false, true);
+      const quota = 'Daily quota limit reached. Quota reset in ';
+      $('#current-temp .loading').text(quota + hours + '.');
+      $('#current-temp > p').text("Daily quota reached.");
+    } else if (msg === "REQUEST_DENIED") {
+      $('#current-temp .loading').text("This request has been denied. Please query a new request.");
+      $('#current-temp > p').text("Request denied");
+    } else if (msg === "INVALID_REQUEST") {
+      $('#current-temp .loading').text("Query is missing. Please enter an address, components of an address or a latitude, longitude in the search box.");
+      $('#current-temp > p').text("Invalid request");
+    } else if (msg === "UNKNOWN_ERROR") {
+      $('#current-temp .loading').text("An unknown error has occurred. Please try again later.");
+      $('#current-temp > p').text("Unknown error");
+    }
   }
-  
-  function convertUnixTime(unix_time, dateStr) {
+
+  function convertUnixTime(unix_time, dateStr, timeTillMidnight) {
     const date = new Date(unix_time * 1000),
           hours = date.getHours(),
           minutes = "0" + date.getMinutes();
     if (dateStr) {
       return date.toDateString();
+    } else if (timeTillMidnight) {
+      return (24 - hours) + 'h' + (60 - minutes.substr(-2));
     } else {
-      return hours + 'h'+ minutes.substr(-2);
+      return hours + 'h' + minutes.substr(-2);
     }
   }
-  
+
   function tempUnitSign() {
     return document.getElementById('radio1').checked ? ' &#176;C' : ' &#176;F';
   }
