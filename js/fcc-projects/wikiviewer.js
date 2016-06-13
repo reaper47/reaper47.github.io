6f09a9683fd4db86a5462f7ef73c679435d3d56d
reaper47 <macpoule@gmail.com>
2016-06-12 23:05:47 -0400

    Started work on a Wikipedia viewer.

---
+++ js/fcc-projects/wikiviewer.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  // 1.0. Set up the language selection select boxes
+  
+  
+  // 1.1. Cache all languages to an array and sort it in alphabetical order
+
+  
+  // 1.2. Populate the language selection select boxes
+  
+  // 2.0. Get a random article when the random button is pressed
+  $('#search-rand').on('click', function() {
+    alert('hi');
+  });
+  
+  // 3.0. Set up the wiki search API to search for any article. Fast mode sends 
+  // an api request after every keyboard input. Slow mode sends a request after a submit.
+  
+  
+});
