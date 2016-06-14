5412f4f4b13fa16f04f59b638ef1d7a19ed3d807
reaper47 <macpoule@gmail.com>
2016-06-13 23:07:33 -0400

    Implemented random search for the most popular languages.

--- js/fcc-projects/wikiviewer.js
+++ js/fcc-projects/wikiviewer.js
@@ -1,16 +1,48 @@
 $(function(){
 
-  // 1.0. Set up the language selection select boxes
-  
-  
-  // 1.1. Cache all languages to an array and sort it in alphabetical order
+  // 1.0. Populate the language selection select boxes
+  let languagesObject, languageList = [];
+  $.ajax({
+    async: false,
+    url: 'languages.json',
+    dataType: 'json',
+    method: 'GET',
+    mimeType: 'application/json',
+    success: function(data) {
+      // 1.1. Populate all languages in an array
+      $.each(data.languages, function(index){
+        languageList.push(index);
+      });
+      languagesObject = data.languages;
 
-  
-  // 1.2. Populate the language selection select boxes
-  
+      // 1.2. Create all options for the language selection form.
+      languageList = languageList.sort();
+      let options = '';
+      for (let i = 0; i < languageList.length; ++i) {
+        if (languageList[i] === "English"){
+          options += '<option value="' + languagesObject[languageList[i]].abv + 
+                     '"selected>' + languageList[i] + '</option>';
+        } else {
+          options += '<option value="' + languagesObject[languageList[i]].abv + 
+                     '">' + languageList[i] + '</option>';
+        }
+      }
+      const selectBox = document.getElementById('language');
+      selectBox.innerHTML = options;
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+
   // 2.0. Get a random article when the random button is pressed
-  $('#search-rand').on('click', function() {
-    alert('hi');
+  $('#search-rand').on('click', function(e) {
+    e.preventDefault();
+    const selectTag = document.getElementById('language').selectedIndex,
+          abv = document.getElementsByTagName('option')[selectTag].value,
+          rand = languagesObject[languageList[selectTag]].rand,
+          randURL = "https://" + abv + "." + "wikipedia.org/wiki/" + rand;
+     window.open(randURL);
   });
   
   // 3.0. Set up the wiki search API to search for any article. Fast mode sends 
