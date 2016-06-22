53f91a6de4fb87712d1634c4057c972b743355e1
reaper47 <macpoule@gmail.com>
2016-06-21 22:08:48 -0400

    Correctly implemented Twitch user fetch and started css work.

--- js/fcc-projects/twitchAPI.js
+++ js/fcc-projects/twitchAPI.js
@@ -1,61 +1,115 @@
 $(function() {
   
-  // 1. Display current kraken twitch streams
-  displayData('https://api.twitch.tv/kraken/streams/')
- 
-  // 2. Display regular twitch users to show some offline accounts
-  const regUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "unofin",
-                    "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"];
+  const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "unofin",
+                 "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", ""];
+                    
+  for (usr in users)
+    displayData('https://api.twitch.tv/kraken/streams/' + users[usr], '#twitch-user-area', usr);
   
-  function displayData(json) {
-    $.getJSON('https://api.twitch.tv/kraken/streams/').done(function(data) {
-
+  $('#online').on('click', function() {
+    $('#online').css('background-color', '#4caf50');
+    $('#all').css('background-color', '#212121');
+    $('#offline').css('background-color', '#212121');
+    $('#online').css('color', '#FFF');
+    $('#offline').css('color', '#212121');
+    $('#all').css('color', '#212121');
+    $('.online').parent().parent().fadeIn(500).show();
+    $('.offline').parent().parent().fadeOut(500).hide();
+    $('.closed').parent().parent().fadeOut(500).hide();
+  });
+  
+  $('#offline').on('click', function() {
+    $('#online').css('background-color', '#212121');
+    $('#all').css('background-color', '#212121');
+    $('#offline').css('background-color', '#ff5722');
+    $('#online').css('color', '#212121');
+    $('#offline').css('color', '#FFF');
+    $('#all').css('color', '#212121');
+    $('.online').parent().parent().fadeOut(500).hide();
+    $('.offline').parent().parent().fadeIn(500).show();
+    $('.closed').parent().parent().fadeIn(500).show();
+  });
+  
+  $('#all').on('click', function() {
+    $('#online').css('background-color', '#212121');
+    $('#all').css('background-color', '#448aff');
+    $('#offline').css('background-color', '#212121');
+    $('#online').css('color', '#212121');
+    $('#offline').css('color', '#212121');
+    $('#all').css('color', '#fff');   
+    $('.online').parent().parent().fadeIn(500).show();
+    $('.offline').parent().parent().fadeIn(500).show();
+    $('.closed').parent().parent().fadeIn(500).show();
+  });
+  
+    
+  function displayData(json, where, num) {
+    $.getJSON(json).done(function(data) { 
+      
       // 1. All twitch data to display are stored in these variables
       let streamStatus = [], streamPreview = [], streamLogo = [], streamURL = [],
-          streamUsr = [], streamGame = [], streamTwitchStatus = [], streamViewers = [],
-          streamLang = []; 
-        
+          streamUsr = [], streamGame = [], streamTwitchStatus = [], 
+          streamViewers = [], streamLang = []; 
+          
       // 2. Store all data to display in their respective arrays
-      $.each(data.streams, function(i, item) {
-        try { streamStatus[i] = item.channel.status; } 
-        catch(err) { streamStatus[i] = null; }
-      });
-      $.each(data.streams, function(i, item) {
-        try { streamPreview[i] = item.preview.large; } 
-        catch(err) { streamPreview[i] = null; }
-      });
-      $.each(data.streams, function(i, item) {
-        try { streamLogo[i] = item.channel.logo; }
-        catch(err) { streamLogo[i] = null; }
-      });
-      $.each(data.streams, function(i, item) {
-        try { streamURL[i] = item.channel.url; }
-        catch(err) { streamURL[i] = null; }
-      });
-      $.each(data.streams, function(i, item) {
-        try { streamUsr[i] = item.channel.display_name; }
-        catch(err) { streamUsr[i] = null; }
-      });
-      $.each(data.streams, function(i, item) { 
-        try { streamTwitchStatus[i] = "online"; }
-        catch(err) { streamTwitchStatus[i] = "offline" };
-      });
-      $.each(data.streams, function(i, item) { 
-        try { streamGame[i] = item.game; }
-        catch(err) { streamGame[i] = null; }
-      });
-      $.each(data.streams, function(i, item) {
-        try { streamViewers[i] = item.viewers; }
-        catch(err) { streamViewers[i] = null; }
-      });
-      $.each(data.streams, function(i, item) {
-        try { streamLang[i] = item.channel.language; }
-        catch(err) { streamLang[i] = null; }
-      });
+      let dataFetch;
+      if (data.streams) dataFetch = data.streams;
+      else dataFetch = data.stream;
+
+      if (dataFetch == null) {
+        let notStreaming = "Currently not streaming.",
+            notStreamingPreview = "https://bit.ly/28JOZxD",
+            notStreamingLogo = "https://bit.ly/28JrBPl",
+            noStreamingGame = "Playing nada :D",
+            notStreamingURL = "https://twitch.tv/";
+        
+        try { streamStatus[0] = dataFetch.channel.status; }
+        catch(err) { streamStatus[0] = notStreaming; }
+        
+        try { streamPreview[0] = dataFetch.preview.large; }
+        catch(err) { streamPreview[0] = notStreamingPreview; }
+        
+        try { streamLogo[0] = dataFetch.channel.logo; }
+        catch(err) { streamLogo[0] = notStreamingLogo; }
+                
+        try { streamURL[0] = dataFetch.channel.url; }
+        catch(err) { streamURL[0] = notStreamingURL + users[num]; }
+        
+        try { 
+          streamUsr[0] = dataFetch.channel.display_name;
+          streamTwitchStatus[0] = "online";
+        }
+        catch(err) { 
+          streamUsr[0] = users[num];
+          streamTwitchStatus[0] = "offline";
+        }   
+        
+        try { streamGame[0] = dataFetch.game; }
+        catch(err) { streamGame[0] = noStreamingGame; }
+        
+        try { streamViewers[0] = dataFetch.viewers; }
+        catch(err) { streamViewers[0] = 0; }
+        
+        try { streamLang[0] = dataFetch.channel.language; }
+        catch(err) { streamLang[0] = "Demonic"; }
+        
+      } else {
+          $.each(dataFetch, function(i, item) {
+            streamStatus[i] = item.channel.status; 
+            streamPreview[i] = item.preview.large;
+            streamLogo[i] = item.channel.logo;
+            streamURL[i] = item.channel.url;
+            streamUsr[i] = item.channel.display_name;
+            streamTwitchStatus[i] = "online";
+            streamGame[i] = item.game;
+            streamViewers[i] = item.viewers;
+            streamLang[i] = item.channel.language;
+          });
+      }
       
       // 3. Append every twitch user profile information to cloned divs
       for (let i = 0; i < streamStatus.length; ++i) {
-        let clone = $('.twitch-users').clone();
+        let clone = $('.twitch-users').clone().removeAttr('id') ;
 
         // 3.1. Modify the hover div info
         const hoverDiv = clone[0].children[0].children[0];
@@ -81,9 +135,59 @@ $(function() {
         bonusInfoDiv.children[0].children[1].textContent = streamLang[i];
         bonusInfoDiv.children[1].children[1].textContent = streamViewers[i];
         
-        $('#twitch-user-area').html(clone);
+        $(where).html(clone);
       }
-      $('.twitch-users')[0].remove();
-  });
+  }).fail(function(err) { 
+      
+      let closedStreaming = "Account closed.";
+      let closedStreamingPreview = "https://bit.ly/28J8J3T";
+      let closedStreamingLogo = "https://bit.ly/28JZ7p0";
+      let closedStreamingGame = "Playing real life.";
+      let closedStreamingURL = "https://en.wikipedia.org/wiki/Reality";
+      
+      let streamStatus = [], streamPreview = [], streamLogo = [], 
+          streamURL = [], streamUsr = [], streamGame = [], 
+          streamTwitchStatus = [], streamViewers = [], streamLang = []; 
+      
+      streamStatus[0] = closedStreaming;
+      streamPreview[0] = closedStreamingPreview;
+      streamLogo[0] = closedStreamingLogo;      
+      streamURL[0] = closedStreamingURL;
+      streamUsr[0] = users[num];
+      streamTwitchStatus[0] = "closed";
+      streamGame[0] = closedStreamingGame;
+      streamViewers[0] = 0;
+      streamLang[0] = "Demonic";
+      
+      for (let i = 0; i < streamStatus.length; ++i) {
+        let clone = $('.twitch-users').clone().removeAttr('id') ;
+
+        // 3.1. Modify the hover div info
+        const hoverDiv = clone[0].children[0].children[0];
+        hoverDiv.alt = streamUsr[i] + "'s channel preview";
+        hoverDiv.src = streamPreview[i];
+        
+        // 3.2. Modify the channel visuals div
+        const channelVisualDiv = clone[0].children[1].children[0];
+        channelVisualDiv.children[0].className += ' ' + streamTwitchStatus[i];
+        channelVisualDiv.children[1].href = streamURL[i];
+        channelVisualDiv.children[1].children[0].alt = (streamUsr[i] + "'s channel");
+        channelVisualDiv.children[1].children[0].src = streamLogo[i];
+        
+        // 3.3. Modify the channel info data
+        const channelInfoData = clone[0].children[1].children[1].children[0];
+        channelInfoData.href = streamURL[i];
+        channelInfoData.children[0].children[0].textContent = streamUsr[i];
+        channelInfoData.children[0].children[1].textContent = streamGame[i];
+        channelInfoData.children[1].textContent = streamStatus[i];
+
+        // 3.4. Modify the bonus information div
+        const bonusInfoDiv = clone[0].children[1].children[2];
+        bonusInfoDiv.children[0].children[1].textContent = streamLang[i];
+        bonusInfoDiv.children[1].children[1].textContent = streamViewers[i];
+        
+        $(where).html(clone);
+      }
+    });
   }
 });
