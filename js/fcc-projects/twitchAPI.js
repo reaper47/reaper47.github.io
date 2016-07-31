ccf59d826747f1b84fc26e027e4f554bcffe351e
reaper99 <macpoule@gmail.com>
2016-07-31 01:05:46 -0400

    Finished work on the Twitch Viewer.

--- js/fcc-projects/twitchAPI.js
+++ js/fcc-projects/twitchAPI.js
@@ -2,47 +2,41 @@ $(function() {
   
   const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "unofin",
                  "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", ""];
-                    
+              
   for (usr in users)
     displayData('https://api.twitch.tv/kraken/streams/' + users[usr], '#twitch-user-area', usr);
   
   $('#online').on('click', function() {
     $('#online').css('background-color', '#4caf50');
-    $('#all').css('background-color', '#212121');
-    $('#offline').css('background-color', '#212121');
+    $('#all, #offline').css('background-color', '#212121');
     $('#online').css('color', '#FFF');
-    $('#offline').css('color', '#212121');
-    $('#all').css('color', '#212121');
+    $('#offline, #all').css('color', '#212121');
     $('.online').parent().parent().fadeIn(500).show();
-    $('.offline').parent().parent().fadeOut(500).hide();
-    $('.closed').parent().parent().fadeOut(500).hide();
+    $('.offline, .closed').parent().parent().fadeOut(500).hide();
+    $('.online').parent().parent().parent().fadeIn(500).show();
+    $('.offline, .closed').parent().parent().parent().fadeOut(500).hide();
   });
   
   $('#offline').on('click', function() {
-    $('#online').css('background-color', '#212121');
-    $('#all').css('background-color', '#212121');
+    $('#online, #all').css('background-color', '#212121');
     $('#offline').css('background-color', '#ff5722');
-    $('#online').css('color', '#212121');
+    $('#online, #all').css('color', '#212121');
     $('#offline').css('color', '#FFF');
-    $('#all').css('color', '#212121');
     $('.online').parent().parent().fadeOut(500).hide();
-    $('.offline').parent().parent().fadeIn(500).show();
-    $('.closed').parent().parent().fadeIn(500).show();
+    $('.offline, .closed').parent().parent().fadeIn(500).show();
+    $('.online').parent().parent().parent().fadeOut(500).hide();
+    $('.offline, .closed').parent().parent().parent().fadeIn(500).show();
   });
   
   $('#all').on('click', function() {
-    $('#online').css('background-color', '#212121');
+    $('#online, #offline').css('background-color', '#212121');
     $('#all').css('background-color', '#448aff');
-    $('#offline').css('background-color', '#212121');
-    $('#online').css('color', '#212121');
-    $('#offline').css('color', '#212121');
+    $('#online, #offline').css('color', '#212121');
     $('#all').css('color', '#fff');   
-    $('.online').parent().parent().fadeIn(500).show();
-    $('.offline').parent().parent().fadeIn(500).show();
-    $('.closed').parent().parent().fadeIn(500).show();
+    $('.online, .offline').parent().parent().fadeIn(500).show();
+    $('.online, .offline').parent().parent().parent().fadeIn(500).show();
   });
   
-    
   function displayData(json, where, num) {
     $.getJSON(json).done(function(data) { 
       
@@ -91,7 +85,7 @@ $(function() {
         catch(err) { streamViewers[0] = 0; }
         
         try { streamLang[0] = dataFetch.channel.language; }
-        catch(err) { streamLang[0] = "Demonic"; }
+        catch(err) { streamLang[0] = "zzz"; }
         
       } else {
           $.each(dataFetch, function(i, item) {
@@ -157,7 +151,7 @@ $(function() {
       streamTwitchStatus[0] = "closed";
       streamGame[0] = closedStreamingGame;
       streamViewers[0] = 0;
-      streamLang[0] = "Demonic";
+      streamLang[0] = "zzz";
       
       for (let i = 0; i < streamStatus.length; ++i) {
         let clone = $('.twitch-users').clone().removeAttr('id') ;
@@ -190,4 +184,55 @@ $(function() {
       }
     });
   }
+    
+  function createUsrCache() {
+    const $twitch_users = $('.twitch-users');
+    let cache = [];
+    
+    $twitch_users.each(function() {
+      if (this.style.display !== 'none') {
+        cache.push({
+          element: this,
+          username: this.children[1].children[1].children[0].children[0].children[0]
+                   .textContent.trim().toLowerCase()
+        }); 
+      }
+    });
+
+    return cache;
+  }
+  
+  function checkCache() {
+    if (!cacheCreated) {
+      userCache = createUsrCache();
+      cacheCreated = true;
+      buttonChanged = false;
+    }
+  }
+  
+  function filter() {
+    const query = this.value.trim().toLowerCase();
+    
+    userCache.forEach(function(usr) {
+      let index = 0;
+      if (query) {
+        index = usr.username.indexOf(query)
+      }
+      usr.element.style.display = index === -1 ? 'none' : '';
+    });
+  }
+  
+  let cacheCreated = false. userCache;
+  $('#searchForm').on('keypress', function(e) { 
+    checkCache(); 
+    $('#search-filter').on('keyup', filter);
+  });
+  
+  $('#searchForm').on('submit', function(e) { e.preventDefault(); });
+    
+  $('#offline, #online, #all').on('click', function() { 
+    console.log('hey');
+    cacheCreated = false;
+    checkCache()
+  });
 });
